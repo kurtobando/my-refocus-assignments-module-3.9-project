46ba632865a6fcb87fb4f837c77e25282ac6c70b
Kurt Obando <kurtobando.developer@gmail.com>
2023-05-30 16:07:20 +0800

    add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import LayoutDefault from "./layouts/LayoutDefault/LayoutDefault";
+import Posts from "./pages/Posts";
+import PostCreate from "./pages/PostCreate";
+import PostRead from "./pages/PostRead";
+import NotFound from "./pages/NotFound";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<LayoutDefault />}>
+      <Route index element={<Posts />} />
+      <Route path="post-create" element={<PostCreate />} />
+      <Route path="post-read/:id" element={<PostRead />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
