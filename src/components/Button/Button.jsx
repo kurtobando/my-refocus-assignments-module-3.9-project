9c3ff39c13281915bf5326a2ccf980c0bee6e2fa
Kurt Obando <kurtobando.developer@gmail.com>
2023-05-31 17:26:54 +0800

    can now toggle favorites

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,11 @@
 import "./Button.css";
 
-function Button({ children, theme = "primary" }) {
-  return <button className={`${theme}`}>{children}</button>;
+function Button({ children, theme = "primary", onClick = () => {} }) {
+  return (
+    <button onClick={onClick} className={`${theme}`}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "../components/Button/Button";
 import ButtonAddPost from "../components/ButtonAddPost/ButtonAddPost";
 import PostsList from "../components/PostsList/PostsList";
 import postsData from "../posts.json";
 
 function Posts() {
-  const [posts, setPosts] = useState(postsData);
+  const [posts, setPosts] = useState([]);
+  const [isFavorites, setIsFavorites] = useState(false);
+
+  useEffect(() => {
+    if (!localStorage.getItem("posts")) {
+      localStorage.setItem("posts", JSON.stringify(postsData));
+    }
+    setPosts(JSON.parse(localStorage.getItem("posts")));
+  }, []);
 
   return (
     <section className="p-1 pt-2 m-auto max-width">
-      {/* call to actions */}
       <div className="flex justify-between">
         <div className="flex" style={{ gap: "0.5rem" }}>
-          <Button theme="primary-rounded">All posts</Button>
-          <Button theme="outline-rounded">Favorites</Button>
+          <Button
+            theme={!isFavorites ? "primary-rounded" : "outline-rounded"}
+            onClick={() => setIsFavorites(false)}
+          >
+            All posts
+          </Button>
+          <Button
+            theme={isFavorites ? "primary-rounded" : "outline-rounded"}
+            onClick={() => setIsFavorites(true)}
+          >
+            Favorites
+          </Button>
         </div>
         <div>
           <Button theme="outline-rounded">
@@ -21,9 +38,9 @@ function Posts() {
           </Button>
         </div>
       </div>
-
-      {/* posts */}
-      <PostsList posts={posts} />
+      <PostsList
+        posts={isFavorites ? posts.filter((post) => post.isLiked) : posts}
+      />
     </section>
   );
 }
